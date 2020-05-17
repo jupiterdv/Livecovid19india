9a0cd0ce0b33d19388dce2104f752f5a9262c20b
Dark Soulz <49095440+jupiterdv@users.noreply.github.com>
2020-05-18 03:53:31 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <meta http-equiv="refresh" content="5; url=http://www.thecodezine.com/covid19/index.php">
+    </head>
+
+    <body>
+        <p>Redirecting in 5 seconds...</p>
+    </body>
+</html>
